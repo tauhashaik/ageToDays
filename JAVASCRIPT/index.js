2a1270b01ec2311742577a5a19daa60d4c070740
tauhashaik <hoosains19@gmail.com>
2023-11-22 14:35:50 +0200

    added css and javascript

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,17 @@
+let input = document.getElementById('input');
+let output = document.getElementById('output');
+let calcbtn = document.getElementById('calcbtn');
+
+calcbtn.addEventListener('click', ageToDays)
+
+function ageToDays(){
+    new Date().getDay()
+    let age = parseInt(ageInput.value);
+    if(!isNaN(age)){
+        let days = age *365;
+    daysOutput.value = days;
+    } else{
+        alert(`Please enter a number you FOOL!`)
+    }
+}
+
